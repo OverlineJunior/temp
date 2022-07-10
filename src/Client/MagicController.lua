5de0693b1e795f4b12e67713b96ffb91afb46a9e
OverlineJunior <franciscosebastianyjunior@hotmail.com>
2022-07-10 20:31:52 -0300

    setup

--- src/Client/MagicController.lua
+++ src/Client/MagicController.lua
@@ -1,32 +1,7 @@
-local CollectionService = game:GetService('CollectionService')
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
 
 local MagicController = Knit.CreateController { Name = 'MagicController' }
-
-
-function MagicController:_AddMagic(tool: Tool)
-    CollectionService:AddTag(tool, 'MagicClient')
-end
-
-
-function MagicController:_RemoveMagic(tool: Tool)
-    CollectionService:RemoveTag(tool, 'MagicClient')
-end
-
-
-function MagicController:KnitStart()
-    local MagicService = Knit.GetService('MagicService')
-
-    MagicService.MagicAdded:Connect(function(...)
-        self:_AddMagic(...)
-    end)
-
-    MagicService.MagicRemoved:Connect(function(...)
-        self:_RemoveMagic(...)
-    end)
-end
-
 
 return MagicController
--- src/Server/MagicService.lua
+++ src/Server/MagicService.lua
@@ -3,34 +3,18 @@ local ReplicatedStorage = game:GetService('ReplicatedStorage')
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
 
-local MagicService = Knit.CreateService {
-    Name = 'MagicService',
-    Client = {
-        MagicAdded = Knit.CreateSignal(),
-        MagicRemoved = Knit.CreateSignal(),
-    },
-}
+local MagicService = Knit.CreateService { Name = 'MagicService' }
 
 
 function MagicService:AddMagic(player: Player)
     local tool = Instance.new('Tool')
     tool.Name = 'Magic'
     tool.Parent = player.Backpack
-
-    CollectionService:AddTag(tool, 'MagicServer')
-
-    self.Client.MagicAdded:Fire(player, tool)
 end
 
 
 function MagicService:RemoveMagic(player: Player)
     local tool = player.Backpack.Magic
-
-    CollectionService:RemoveTag(tool, 'MagicServer')
-
-    -- When commented out, the expected behavior is for only the MagicServer component to be removed, but for some reason,
-    -- the MagicClient component is also being removed.
-    self.Client.MagicRemoved:Fire(player, tool)
 end
 
 
